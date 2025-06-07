1d55b71dc0593807e6bd8384525cc12cfb9624d0
Machad3x <machad3x@gmail.com>
2025-06-07 10:35:39 -0400

    dm2q: PixelOS bringup

--- aosp_dm2q.mk
+++ aosp_dm2q.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from the device configuration.
 $(call inherit-product, device/samsung/dm2q/device.mk)
 
-# Inherit from the Lineage configuration.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from the AOSP configuration.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_dm2q
+PRODUCT_NAME := aosp_dm2q
 PRODUCT_DEVICE := dm2q
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-S9160
